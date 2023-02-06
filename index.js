c6ed46ed0b1d0af2418d70623eb074de6e85439e
Matheus <kroskamatheus@gmail.com>
2023-02-06 13:34:56 -0300

    postgre setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+
+const pool = new Pool({
+  user: "mhkroska",
+  host: "localhost",
+  database: "devnology",
+  password: "12345",
+  port: 5432,
+});
+
+app.get("/", (req, res) => {
+  pool.query("SELECT * FROM users", (err, result) => {
+    if (err) {
+      res.status(500).send(err.toString());
+    } else {
+      res.send(JSON.stringify(result.rows));
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000!");
+});
