274c512ef7792cde259c3537be80b08d8a0987fb
Matheus <kroskamatheus@gmail.com>
2023-02-07 00:08:14 -0300

    creating routes (orders & customers)

--- index.js
+++ index.js
@@ -11,12 +11,54 @@ const pool = new Pool({
   port: 5432,
 });
 
-app.get("/", (req, res) => {
-  pool.query("SELECT * FROM users", (err, result) => {
+app.post("/customers", (req, res) => {
+  const { name, email, password } = req.body;
+  pool.query(
+    "INSERT INTO customers (name, email, password) VALUES ($1, $2, $3)",
+    [name, email, password],
+    (err, result) => {
+      if (err) {
+        res.status(500).send(err.toString());
+      } else {
+        res.send("Customer added successfully!");
+      }
+    }
+  );
+});
+
+app.get("/customers/:id", (req, res) => {
+  const { id } = req.params;
+  pool.query("SELECT * FROM customers WHERE id = $1", [id], (err, result) => {
     if (err) {
       res.status(500).send(err.toString());
     } else {
-      res.send(JSON.stringify(result.rows));
+      res.send(JSON.stringify(result.rows[0]));
+    }
+  });
+});
+
+app.post("/orders", (req, res) => {
+  const { customer_id, date, payment_method } = req.body;
+  pool.query(
+    "INSERT INTO orders (customer_id, date, payment_method) VALUES ($1, $2, $3)",
+    [customer_id, date, payment_method],
+    (err, result) => {
+      if (err) {
+        res.status(500).send(err.toString());
+      } else {
+        res.send("Order added successfully!");
+      }
+    }
+  );
+});
+
+app.get("/orders/:id", (req, res) => {
+  const orderId = req.params.id;
+  pool.query("SELECT * FROM orders WHERE id = $1", [orderId], (err, result) => {
+    if (err) {
+      res.status(500).send(err.toString());
+    } else {
+      res.send(JSON.stringify(result.rows[0]));
     }
   });
 });
