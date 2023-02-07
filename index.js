09dcb4f4d083b64b98a09e6b282960a87eab9464
Matheus <kroskamatheus@gmail.com>
2023-02-06 22:58:05 -0300

    removing create table

--- index.js
+++ index.js
@@ -11,17 +11,6 @@ const pool = new Pool({
   port: 5432,
 });
 
-pool.query(
-  "CREATE TABLE users (id serial PRIMARY KEY, username VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, email VARCHAR(355) UNIQUE NOT NULL)",
-  (err, result) => {
-    if (err) {
-      console.error(err.toString());
-    } else {
-      console.log("Tabela de usuários criada com sucesso!");
-    }
-  }
-);
-
 app.get("/", (req, res) => {
   pool.query("SELECT * FROM users", (err, result) => {
     if (err) {
